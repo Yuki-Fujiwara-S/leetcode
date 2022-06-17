276f9f7d35936eed0be462c8227f120bba62de2f
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-06-16 18:13:57 -0700

    initial

--- udemy/bigOnotation.js
+++ udemy/bigOnotation.js
@@ -3,3 +3,5 @@
 
 // Big O notation is a mathematical notation that describes the limiting behavior of a 
 // function when the argument tends towards a particular value or infinity.
+
+// Time complexity
