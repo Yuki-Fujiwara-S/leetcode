e610b441d01b23bc6f840274d50af3765c5549cc
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-06-27 18:18:12 -0700

    solution with comparing values and recursion, using higher lower

--- trees/LCAofBST.js
+++ trees/LCAofBST.js
@@ -12,3 +12,30 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+
+ var lowestCommonAncestor = function(root, p, q) {
+  // ask clarifying questions - constraints
+  
+  // store values?
+  // Use bounds?
+  
+  if (!root) {
+      return null;
+  }
+  
+  let higher = Math.max(p.val, q.val);
+  let lower = Math.min(p.val, q.val);
+  
+  if (root.val >= lower && root.val <= higher) {
+      return root;
+  }
+  
+  if (root.val > higher) {
+      return lowestCommonAncestor(root.left, p, q);
+  }
+  
+  if (root.val < lower) {
+      return lowestCommonAncestor(root.right, p, q);
+  }
+  
+};
