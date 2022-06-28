edf57fa3f1015a1854c9a14f9f4b90e3ce115602
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-06-27 18:16:14 -0700

    add solution using helper and recursion

--- trees/subTreeofAnotherTree.js
+++ trees/subTreeofAnotherTree.js
@@ -11,3 +11,45 @@
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
+
+ var isSubtree = function(root, subRoot) {
+  // traverse root
+  // use null property at the end to check equality
+  
+  if(!root) {
+      return false;
+  }
+  
+  if(isSameTree(root, subRoot)) {
+      return true;
+  }
+      
+  let leftCheck = isSubtree(root.left, subRoot);
+  let rightCheck = isSubtree(root.right, subRoot);
+  
+  return leftCheck || rightCheck;
+  
+};
+
+var isSameTree = function(p, q) {
+  
+  if (!p && !q) {
+      return true;
+  }
+  
+  if (!p || !q) {
+      return false;
+  }
+  
+  if (p.val !== q.val) {
+      return false;
+  }
+    
+  // need to return true when done checking all nodes - how to use recursion majik?
+  // does if !p and !q case work for returning true; 
+  let leftCheck = isSameTree(p.left, q.left); 
+  let rightCheck = isSameTree(p.right, q.right);
+  
+  return leftCheck && rightCheck;
+  
+};
