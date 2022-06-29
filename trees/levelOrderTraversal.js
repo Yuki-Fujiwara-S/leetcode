a582b324cc4f0c20201a9400279e0e35dd8ea988
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-06-28 17:13:54 -0700

    bfs traversal solution

--- trees/levelOrderTraversal.js
+++ trees/levelOrderTraversal.js
@@ -13,3 +13,34 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+
+ var levelOrder = function(root) {
+  // left and right  
+  if (!root) {
+      return [];
+  }
+  
+  // bfs
+  let queue = [root];
+  let result = [];
+  
+  while(queue.length) {
+      let subArr = [];
+      let length = queue.length;
+      
+      for (let i = 0; i < length; i++) {
+          let elm = queue.shift();
+          subArr.push(elm.val);
+          
+          //need to add children to queue
+          if (elm.left) {
+              queue.push(elm.left);
+          }
+          if (elm.right) {
+              queue.push(elm.right);
+          }
+      }
+      result.push(subArr);
+  }
+  return result;
+};
