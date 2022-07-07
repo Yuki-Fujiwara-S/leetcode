9219e0d7c11fd84ba7837d34f95ae4ee35e29ecb
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-07-06 17:50:34 -0700

    solution using recursion and complex logic involving comparison of subtree with paths, as well as math.max with 0

--- trees/binaryTreeMaxPath.js
+++ trees/binaryTreeMaxPath.js
@@ -4,3 +4,18 @@
 
 // Given the root of a binary tree, return the maximum path sum of any non-empty path.
 
+var maxPathSum = function(root) {
+  let max = -999999;
+ function maxGain(node){
+      if(!node) return 0;
+      const leftGain = Math.max( maxGain(node.left),0);
+      const rightGain = Math.max( maxGain(node.right),0);
+
+      let subPath =  node.val + leftGain + rightGain;
+      max = Math.max(subPath,max)
+
+      return node.val + Math.max(leftGain,rightGain)
+ }
+  maxGain(root)
+  return max
+};
