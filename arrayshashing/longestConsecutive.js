ff43758816a4209130f96e5d48b6524d9375b430
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-07-21 18:57:50 -0700

    sorting solution. not o(n)

--- arrayshashing/longestConsecutive.js
+++ arrayshashing/longestConsecutive.js
@@ -1,3 +1,38 @@
 // Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 
 // You must write an algorithm that runs in O(n) time.
+
+// no O(n) solution , nlog(n)
+// var longestConsecutive = function(nums) {
+//     // can see screen
+//     // 
+//     // check longest
+//     if (nums.length === 0) {
+//         return 0;
+//     }
+    
+//     let sorted = nums.sort(function(a,b) {
+//         return a - b;
+//     });
+    
+//     let sortedSet = [...new Set(sorted)]
+    
+    
+//     let counter = 1;
+//     let largestsofar = 0;
+    
+//     for (let i = 0; i < sortedSet.length - 1; i++) {
+//         if (sortedSet[i + 1] - sortedSet[i] === 1) {
+//             counter += 1;
+//         } else {
+//             if (counter > largestsofar) {
+//                 largestsofar = counter;
+//             }
+//             counter = 1;
+//         }
+        
+//     }
+    
+//     return largestsofar > counter ? largestsofar : counter;
+
+// };
