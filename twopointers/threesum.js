2a70ad75639bb3854b54a30e51f64868c1a097e7
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-07-27 17:33:48 -0700

    solution using for loop with nested while loop

--- twopointers/threesum.js
+++ twopointers/threesum.js
@@ -2,3 +2,48 @@
 
 // Notice that the solution set must not contain duplicate triplets.
 
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var threeSum = function(nums) {
+    
+    
+  let result = [];
+  
+  let sorted = nums.sort(function(a,b) {
+      return a - b;
+  })
+  
+  for (let i = 0; i < sorted.length; i++)  {
+      if (i !== 0 && sorted[i] === sorted[i - 1]) {
+          continue;
+      }
+      
+      let start = i + 1;
+      let end = sorted.length - 1;
+      let target = sorted[i];
+      
+      while (start < end) {
+          let numsum = nums[start] + nums[end];
+          if (numsum + target === 0) {
+              result.push([nums[start], nums[i], nums[end]]);
+              while (nums[start] === nums[start + 1]) {
+                  start++;
+              }
+              while (nums[end] === nums[end - 1]) {
+                  end--;
+              }
+              start++;
+              end--;
+          } else if (numsum + target > 0) {
+              end--;
+          } else {
+              start++;
+          }
+      }    
+  }
+  
+  return result;
+  
+};
