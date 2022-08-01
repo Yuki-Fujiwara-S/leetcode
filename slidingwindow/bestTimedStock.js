1162c6d38d8b46e27e0d750ebb731ab8056ce198
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-08-01 15:41:03 -0700

    solution using sliding window

--- slidingwindow/bestTimedStock.js
+++ slidingwindow/bestTimedStock.js
@@ -4,3 +4,29 @@
 
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+  // what i s a s l i d i n g window
+  // what is mcFred
+  // [2, 1000, 1, 3, 4, 6]
+  
+  let profitSoFar = 0;
+  
+  let minSoFar = Number.MAX_VALUE;
+  
+  for (let i = 0; i < prices.length; i++) {
+      minSoFar = Math.min(prices[i], minSoFar);
+      let profit = prices[i] - minSoFar;
+      
+      profitSoFar = Math.max(profit, profitSoFar);
+  }
+  
+  return profitSoFar;
+  
+  
+
+  
+};
