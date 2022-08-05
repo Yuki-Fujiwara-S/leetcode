c1a8da25b3d75f3ffce21f58d97f6df2aaa725eb
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-08-05 16:56:58 -0700

    solution suign stack

--- stack/validParentheses.js
+++ stack/validParentheses.js
@@ -4,3 +4,39 @@
 
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+  // Queue - FIFO
+  // Stack - LIFO
+  // pop off anything from the stack
+      
+  let stack = [];
+  
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] === ")") {
+          if (stack.pop() !== "(") {
+              console.log(stack[-1]);
+              return false;
+          }
+      } else if (s[i] === "}") {
+          if (stack.pop() !== "{") {
+              return false;
+          }
+      } else if (s[i] === "]") {
+          if (stack.pop() !== "[") {
+              return false;
+          }
+      } else {
+          stack.push(s[i]);
+      }
+      
+  }
+  
+  return stack.length === 0;
+   
+  
+};
