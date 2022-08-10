392c1d4de84b5cff3cf50e1382ef14b93418f727
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-08-09 17:22:35 -0700

    proceed with logic using if checks for operators

--- stack/RevesePolishNotation.js
+++ stack/RevesePolishNotation.js
@@ -13,8 +13,51 @@
  var evalRPN = function(tokens) {
   let stack = [];
   let totalSoFar = 0;
+  let first = true;
   
   //lhs rhs?
+  for (let i = 0; i < tokens.length; i++) {
+      // ADDITION
+      if (tokens[i] === "+") {
+         if (first) {
+             totalSoFar = stack.pop() + stack.pop();
+             first = false;
+         } else {
+             totalSoFar += stack.pop();
+         }
+          continue;   
+      }
+      
+      // SUBTRACTION
+      if (tokens[i] === "-") {
+         if (first) {
+             tokenOne = stack.pop();
+             tokenTwo = stack.pop();
+             totalSoFar = tokenOne - tokenTwo;
+             first = false;
+         } else {
+             totalSoFar = stack.pop() - totalSoFar;
+         }
+          continue;   
+      }
+      
+      // MULTIPLICATION
+      if (tokens[i] === "*") {
+         if (first) {
+             totalSoFar = stack.pop() * stack.pop();
+             first = false;
+         } else {
+             totalSoFar *= stack.pop();
+         }
+          continue;   
+      }
+      
+      
+      
+      // push to stack
+      stack.push(tokens[i]);
+  }
   
+  return totalSoFar;
   
 };
