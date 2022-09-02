d7e8a231da1b4f7de70e91625d085f1f53f57172
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-02 15:23:48 -0700

    recursive solution

--- linkedlist/reverseLinkedList.js
+++ linkedlist/reverseLinkedList.js
@@ -23,3 +23,15 @@
   
   return prev;
 };
+
+
+// recursive solution
+function reverseList(head) {
+  if (!head || !head.next) {
+      return head;
+  }
+  var newHead = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return newHead;
+}
