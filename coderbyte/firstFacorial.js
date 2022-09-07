ce7a9649b175d2fffab9655f3028e47b30c1ea4e
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-06 18:31:37 -0700

    brute force solution using for loop

--- coderbyte/firstFacorial.js
+++ coderbyte/firstFacorial.js
@@ -1,6 +1,11 @@
-function FirstFactorial(num) { 
+function FirstFactorial(num) {
+  let result = 1;
+
+  for (let i = 1; i < num + 1; i++) {
+    result = result * i;
+  }
 
   // code goes here  
-  return num; 
+  return result; 
 
 }
