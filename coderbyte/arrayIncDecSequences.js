270cc5c110e8a3abc1da306a14188b582cf22b1c
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-07 18:51:12 -0700

    add increasing conditional statements

--- coderbyte/arrayIncDecSequences.js
+++ coderbyte/arrayIncDecSequences.js
@@ -1,2 +1,12 @@
 // return index at which increasing - > decreasing and vice verca, return -1 if continues the same
 
+const findIndex = (arr) => {
+  let increasing;
+  
+  if (arr[0] < arr[1]) {
+    increasing = true;
+  } else {
+    increasing = false;
+  }
+
+}
