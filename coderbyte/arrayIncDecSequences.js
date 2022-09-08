b5346dd2d8a8241e3cef02f2e72a0dc9b73c6201
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-07 19:01:20 -0700

    add for loop logic, tests passed

--- coderbyte/arrayIncDecSequences.js
+++ coderbyte/arrayIncDecSequences.js
@@ -9,12 +9,30 @@ const findIndex = (arr) => {
     increasing = false;
   }
 
-  
+  if (increasing && arr[arr.length - 2] < arr[arr.length - 1]) {
+    return -1;
+  }
 
+  if (!increasing && arr[arr.length - 2] > arr[arr.length - 1]) {
+    return -1;
+  }
 
+  for (let i = 0; i < arr.length; i++) {
+    if (increasing) {
+      if (arr[i] > arr[i + 1]) {
+        return i;
+      }
+    }
 
+    if (!increasing) {
+      if (arr[i] < arr[i + 1]) {
+        return i;
+      }
+    }
+  }
 }
 
 // tests
 console.log(findIndex([1, 2, 3, 4, -1, -2]), "should be 3");
 console.log(findIndex([1, 2, 3, 4, 5, 6]), "should be -1");
+console.log(findIndex([2, 1, 0]), "should be -1");
