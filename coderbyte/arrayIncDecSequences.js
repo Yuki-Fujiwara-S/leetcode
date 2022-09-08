b689902433ee9da31ee70c52e3f8eae9a0b8a311
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-07 18:57:25 -0700

    add manual tests

--- coderbyte/arrayIncDecSequences.js
+++ coderbyte/arrayIncDecSequences.js
@@ -9,4 +9,12 @@ const findIndex = (arr) => {
     increasing = false;
   }
 
+  
+
+
+
 }
+
+// tests
+console.log(findIndex([1, 2, 3, 4, -1, -2]), "should be 3");
+console.log(findIndex([1, 2, 3, 4, 5, 6]), "should be -1");
