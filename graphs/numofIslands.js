640607b285c018df878afe71b3edc6eebea73eda
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-11 16:35:01 -0700

    redo numOfIslands from scratch, utilize recursion and nested for loop

--- graphs/numofIslands.js
+++ graphs/numofIslands.js
@@ -3,34 +3,43 @@
  * @return {number}
  */
  var numIslands = function(grid) {
-  //
-  let counter = 0;
-  for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid[0].length; j++) {
-          //
-          if (grid[i][j] == '1') {
-              counter += 1;
-              depthFirstSearch(grid, i, j);
-          }
-      }
-  }
-  return counter;
+    // counter
+    let counter = 0;
+    
+    // need a nester forloop to target each element
+    
+    for (let i = 0; i < grid.length; i++){
+        for (let j = 0; j < grid[i].length; j++) {
+            // check if land
+            if(grid[i][j] === "1"){
+                counter++;
+                dfs(grid, i, j);
+            }
+        }
+    }
+    return counter;
+    
 };
 
-const depthFirstSearch = function(grid, i, j) {
-  //check bottom node, update to 0 if 1
-  //check right node, update to 0 if 1
-  if ( i >= grid.length || j >= grid[0].length || i < 0 || j < 0) {
-      return;
-  }
-  if (grid[i][j] == '0') {
-      return;
-  }
-  
-  grid[i][j] = '0';
-  depthFirstSearch(grid, i + 1, j );
-  depthFirstSearch(grid, i, j + 1);
-  depthFirstSearch(grid, i - 1, j);
-  depthFirstSearch(grid, i, j - 1);
-  
+const dfs = function(grid, i, j) {
+    let column = grid.length;
+    let row = grid[0].length;
+    // make sure the search remains within the bounds
+    if (i < 0 || j < 0 || i >= column || j >= row) {
+        return;
+    }
+    
+    if (grid[i][j] === "0") {
+        return;
+    }
+    
+    // convert land to water once done with exploration
+    grid[i][j] = "0";
+    
+    // recursive call to do exploration
+    dfs(grid, i, j + 1) // right
+    dfs(grid, i, j - 1) // left
+    dfs(grid, i + 1, j) // down
+    dfs(grid, i - 1, j) // up
+    
 }
