bba262bd22c0204c53f3eb1973ae9a6e68b98fb8
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-13 17:23:28 -0700

    initial commit

---
+++ parkinglotprep/spareBus.js
@@ -0,0 +1,8 @@
+// We are developing an application for bus drivers that tells them where to drive, so that they can pick up and drop off riders that have been matched to them. A primary function of this app is to display a set of "driver instructions" that tells the driver where to drive and who to pick up or drop off over the course of the entire day.
+ 
+//  Each bus has a limited capacity in terms of the maximum number of riders that can be on board simultaneously. Your task is to write code that takes as input some driver instructions, and tells us whether following those instructions would cause the bus's capacity to be exceeded at any point in the day.
+ 
+//  Follow up:
+ 
+//  Now, we learn that some riders are on wheelchairs. Each wheelchair requires two seats to be folded up on the bus when such a rider boards, and when the rider exits the bus those two seats become free again. We know ahead of time which riders will require wheelchairs. However, not every seat on a bus is foldable, and some buses have more foldable seats than others. How would you modify your solution to handle this functionality?
+
