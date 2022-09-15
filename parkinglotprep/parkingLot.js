08c29f02a8ea2d5511a073e36f7343e05b76b1d2
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-14 17:32:31 -0700

    add solution using return statement boolean

--- parkinglotprep/parkingLot.js
+++ parkinglotprep/parkingLot.js
@@ -1,1 +1,24 @@
 // Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
+
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+ var ParkingSystem = function(big, medium, small) {
+  this.count = [big, medium, small]
+};
+
+/** 
+* @param {number} carType
+* @return {boolean}
+*/
+ParkingSystem.prototype.addCar = function(carType) {
+  return this.count[carType - 1]-- > 0;
+};
+
+/** 
+* Your ParkingSystem object will be instantiated and called as such:
+* var obj = new ParkingSystem(big, medium, small)
+* var param_1 = obj.addCar(carType)
+*/
