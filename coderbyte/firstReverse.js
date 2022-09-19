55bd990ebb1e68af4ce8638948b81f87286a609b
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-19 15:32:44 -0700

    solution using reversed for loop

--- coderbyte/firstReverse.js
+++ coderbyte/firstReverse.js
@@ -1,8 +1,10 @@
 function FirstReverse(str) { 
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i]; 
+  }
 
-  // code goes here  
-  return str; 
-
+  return reversed;
 }
    
 // keep this function call here 
