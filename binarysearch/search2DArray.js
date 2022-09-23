c4f2813e5492a817b3669f52776bf8c72c1f45b5
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-09-22 17:22:25 -0700

    add comments

--- binarysearch/search2DArray.js
+++ binarysearch/search2DArray.js
@@ -10,5 +10,10 @@
  * @return {boolean}
  */
  var searchMatrix = function(matrix, target) {
-    
+  // we want to find largest number less than target on the first element in subarray
+  // we want to find smallest number greater than target on the last element in subarray
+  
+  // want to check start and end of subarrays once we find range
+  
+  
 };
